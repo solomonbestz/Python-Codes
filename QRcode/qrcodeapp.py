7fc3464da0cf26f1ff65ed0a4f5a40b15ebea5e4
solomonugo <kingsolodawarrior@gmail.com>
2022-03-03 22:21:20 +0100

    qrcodeapp with python kivy

---
+++ QRcode/qrcodeapp.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.lang import Builder 
+
+
+
+class QrCodeApp(App):
+
+    def build(self):
+        return Builder.load_string(
+"""
+#: import ZBarCam kivy_garden.zbarcam.ZBarCam
+BoxLayout:
+    orientation: 'vertical'
+    ZBarCam:
+        id: qrcodecam
+    Label:
+        size_hint: None, None
+        size: self.texture_size[0], 50
+        text: ' '.join([str(symbol) for symbol in qrcodecam.symbols]) 
+"""
+)
+
+
+if __name__=='__main__':
+    QrCodeApp().run()
