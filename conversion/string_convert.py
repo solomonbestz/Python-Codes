acc0625c34c0a1d65b85788fe300ef86b706d981
Solomon Bestz <kingsolodawarrior@gmail.com>
2022-06-20 10:11:35 +0100

    convert string to all capital if it contains 2 uppercase in first 4 characters

---
+++ conversion/string_convert.py
@@ -0,0 +1,14 @@
+
+def convert(word):
+    count_capital = 0
+    for char in range(len(word)):
+        if word[char].isupper():
+            count_capital += 1
+        if char == 3 and count_capital == 2:
+            print(word.upper())
+            break     
+    print(f"Found {count_capital} capital letter not within the range of the first four characters")
+        
+if __name__=='__main__':
+    string = 'SolomOn'
+    convert(string)
